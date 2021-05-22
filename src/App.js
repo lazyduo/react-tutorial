974e7d935aeecb8761a76f164d6b4ed2d790f534
lazyduo <dadanuna@gmail.com>
2021-05-23 00:25:57 +0900

    MyName class import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
+import MyName from './MyName';
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* comment */}
-        Hi there~
-      </div>
-    )
+      <MyName name="리액트" />      
+    );
   }
 }
 
